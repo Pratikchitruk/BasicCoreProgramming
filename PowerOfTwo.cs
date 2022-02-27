ffe61779a4ca67a97c023030553cec5a2130c902
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-02-27 20:28:32 +0530

    for basic programming

---
+++ PowerOfTwo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    public class Power
+    {
+       public static void PowerOfTwo()
+        {
+            Console.WriteLine(IsPowerOfTwo(9223372036854775809));
+            Console.WriteLine(IsPowerOfTwo(4));
+            Console.ReadLine();
+        }
+        static bool IsPowerOfTwo(ulong n)
+        {
+            if (n == 0)
+                return false;
+            while (n != 1)
+            {
+                if (n % 2 != 0)
+                    return false;
+                n = n / 2;
+            }
+            return true;
+        }
+    }
+}
