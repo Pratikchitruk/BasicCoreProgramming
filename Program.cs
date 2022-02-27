7ee44bf0df7485f78291133e745c3ab53f41ae8c
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-27 20:21:46 +0530

    program.cs push for main

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using FlippingCoin;
+using System;
+
+namespace BasicCoreProgramming
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BasicCoreProgramming");
+            FlipCoin.FlipTheCoin();
+            LeapYear.FindLeapYear();
+            Power.PowerOfTwo();
+            HarmonicNumber.FindTheHarmonicNo();
+            factors.findThefactors();
+
+        }
+    }
+}
